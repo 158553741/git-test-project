c6a9451111f0beda4778ed6128072c3603c73f54
liujunjie <158553741@qq.com>
2021-10-19 10:01:02 +0800

    初始化分支

--- src/com/future/git/gitClasss.java
+++ src/com/future/git/gitClasss.java
@@ -3,5 +3,6 @@ package com.future.git;
 public class gitClasss {
     public static void main(String[] args) {
         System.out.println("git test allot");
+        System.out.println("git test allot");
     }
 }
