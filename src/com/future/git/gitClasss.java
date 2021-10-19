19ded229d8a8ac59d4f91e40c7ee132ac821dc45
liujunjie <158553741@qq.com>
2021-10-19 10:05:42 +0800

    初始化增加输出

--- src/com/future/git/gitClasss.java
+++ src/com/future/git/gitClasss.java
@@ -4,5 +4,8 @@ public class gitClasss {
     public static void main(String[] args) {
         System.out.println("git test allot");
         System.out.println("git test allot");
+        System.out.println("git test allot");
+        System.out.println("git test allot"); System.out.println("git test allot");
+
     }
 }
